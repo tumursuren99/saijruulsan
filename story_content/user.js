77a153d3b7f93231d42ffe03685eadddedad0b47
tumuruu <tumursuren99@outlook.com>
2021-07-17 15:32:08 +0800

    saijruulsan

---
+++ story_content/user.js
@@ -0,0 +1,33 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5udHLxTMQ79":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var surname = player.GetVar('surname');
+var name = player.GetVar('name');
+var email = player.GetVar('email');
+var status = player.GetVar('status');
+var score = player.GetVar('score');
+
+var formData = new FormData();
+formData.append('Surname', surname);
+formData.append('Name', name);
+formData.append('Email', email);
+formData.append('Status', status);
+formData.append('Score', score);
+//formData.append('Some text', 'Here is some text');
+
+var request = new XMLHttpRequest();
+request.open('POST', 'https://script.google.com/macros/s/AKfycbzbgCuyyLghHbnz0sOdQ6jx9Z9VZis-bS-0absx0cTqkjov1lAaRchDz0f9RFIHYXevYA/exec');
+request.send(formData);
+}
+
